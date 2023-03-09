3e6d74a0524352814c0bd56d39d57b79861e8370
shilnara dam <shilnarawdam@gmail.com>
2023-03-08 16:28:35 -0800

    added two versions of firebase config file object export.

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,3 +1,5 @@
+/*
+//use this for running script
 module.exports = {
     apiKey: "AIzaSyD_xLu-bgWtlvtcs6pEkkxilc-gFJY7tvo",
     authDomain: "vue-3-mastery-forum.firebaseapp.com",
@@ -6,3 +8,15 @@ module.exports = {
     messagingSenderId: "526820541888",
     appId: "1:526820541888:web:8c6cf1fd02288f9212b425"
 };
+*/
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD_xLu-bgWtlvtcs6pEkkxilc-gFJY7tvo",
+    authDomain: "vue-3-mastery-forum.firebaseapp.com",
+    projectId: "vue-3-mastery-forum",
+    storageBucket: "vue-3-mastery-forum.appspot.com",
+    messagingSenderId: "526820541888",
+    appId: "1:526820541888:web:8c6cf1fd02288f9212b425"
+};
+
+export { firebaseConfig };
