f18bfd0fcad8bb4c7d4f08e269f4ebe9903bc8ce
shilnara dam <shilnarawdam@gmail.com>
2023-04-05 15:39:26 -0700

    Trying vuetify treeshaking.

--- src/plugins/Vuetify.ts
+++ src/plugins/Vuetify.ts
@@ -1,14 +1,10 @@
 //vuetify stuff
 import { createVuetify } from "vuetify";
-import * as components from "vuetify/components";
-import * as directives from "vuetify/directives";
 import "vuetify/styles";
 
 //font awesome stuff
 
 const vuetify = createVuetify({
-    components,
-    directives,
     theme: {
         defaultTheme: "light" //requires changing alot of things due to using external css and stuff
     }
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { fileURLToPath, URL } from "node:url";
 
 import vue from "@vitejs/plugin-vue";
+import vuetify from "vite-plugin-vuetify";
+
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue()],
+    plugins: [vue(), vuetify()],
     resolve: {
         alias: {
             "@": fileURLToPath(new URL("./src", import.meta.url))
